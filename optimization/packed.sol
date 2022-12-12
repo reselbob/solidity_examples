c573c748238d90dc120cc3856eeb8a2ce7f6e9a6
reselbob <bob@CogArtTech.com>
2022-12-11 22:00:25 -0800

    streamlining packed and unpacked

--- optimization/packed.sol
+++ optimization/packed.sol
@@ -2,33 +2,16 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Packed {
-    struct Optimized {
-         uint8 a;
-        uint8 b;
-        uint8 c;
-        uint8 d;
-        bytes16 e;
-        bytes16 h;
-        bytes32 f;
-        bytes32 g;
-    }
-
-   struct Optimized2 {
+   struct Optimized {
         uint64 a;
         uint64 b;
         uint256 c;
     }
 
     Optimized example;
-    Optimized2 example2;
     
    function fillStruct() public {
-        Optimized memory someStruct = Optimized(1,2,3,4,"a","b","c","d");
+        Optimized memory someStruct = Optimized(100,200, 400);
         example = someStruct;
-    }
-
-    function fillStruct2() public {
-        Optimized2 memory someStruct = Optimized2(100,200,400);
-        example2 = someStruct;
     }
 }
--- optimization/unpacked.sol
+++ optimization/unpacked.sol
@@ -3,32 +3,15 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Unpacked {
     struct NotOptimized {
-        uint8 a;
-        uint8 b;
-        uint8 c;
-        uint8 d;
-        bytes32 e;
-        bytes16 h;
-        bytes32 f;
-        bytes16 g;
-    }
-
-    struct NotOptimized2 {
         uint64 a;
         uint256 b;
         uint64 c;
     }
 
     NotOptimized example;
-    NotOptimized2 example2;
     
     function fillStruct() public {
-        NotOptimized memory someStruct = NotOptimized(1,2,3,4,"a","b","c","d");
+        NotOptimized memory someStruct = NotOptimized(100, 400, 200);
         example = someStruct;
-    }
-
-    function fillStruct2() public {
-        NotOptimized2 memory someStruct = NotOptimized2(100,400,200);
-        example2 = someStruct;
     }
 }
