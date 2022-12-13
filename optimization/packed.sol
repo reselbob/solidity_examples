3be90f54e3007010dd0510d7dc84322a465a1e67
reselbob <bob@CogArtTech.com>
2022-12-12 18:02:44 -0800

    streamlining file system

--- optimization/packed.sol
+++ optimization/packed.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Packed {
    struct Optimized {
-        uint64 a;
-        uint64 b;
-        uint256 c;
+        uint64 number_1;
+        uint64 number_3;
+        uint256 number_2;
     }
 
     Optimized example;
--- optimization/unpacked.sol
+++ optimization/unpacked.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Unpacked {
     struct NotOptimized {
-        uint64 a;
-        uint256 b;
-        uint64 c;
+        uint64 number_1;
+        uint256 number_2;
+        uint64 number_3;
     }
 
     NotOptimized example;
