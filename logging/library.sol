8c05a32d778b72b7330fc9216ce3d51ebf087493
Bob Reselman <bob@CogArtTech.com>
2022-12-17 15:47:50 -0800

    Update library.sol

--- logging/library.sol
+++ logging/library.sol
@@ -1,1 +1,10 @@
-TO BE PROVIDED
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+library LibOperations {
+    function increment(
+      uint256 a) public view returns (
+      uint) {
+        return (a + 1);
+    }
+}
