ceb2da69f081aa442250ebad1b7167c65dd00a16
Bob Reselman <bob@CogArtTech.com>
2022-12-17 15:41:37 -0800

    Update simple.sol

--- logging/simple.sol
+++ logging/simple.sol
@@ -1,1 +1,9 @@
-TO BE PROVIDED
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Simple {
+    uint256 grand_total;
+    
+    function increment() public {
+    }
+}
