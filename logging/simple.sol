a662d4843ae51429a260882a9d21ea5eb994ce8c
reselbob <bob@CogArtTech.com>
2022-12-17 17:38:23 -0800

    Added contract owner

--- logging/simple.sol
+++ logging/simple.sol
@@ -1,16 +1,32 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import {LibOperations} from "logging/library.sol";
-
 contract Simple {
     
-    using LibOperations for uint256;
+    address public contract_owner;
+
+    constructor(){
+        contract_owner = msg.sender;
+    }
     
-    uint256 public grand_total;
-    
-    function update() public {
-        uint a = grand_total;
-        grand_total= a.increment(msg.sender);
+    struct MyStruct {
+        uint64 number_1;
+        uint64 number_2;
+        uint64 number_3;
+    }
+
+     MyStruct public a_struct;
+
+    event BeforeIncrement(address indexed _from, MyStruct _value);
+    event AfterIncrement(address indexed _from, MyStruct _value);
+
+    function updateUint() public {
+        emit BeforeIncrement(msg.sender, a_struct);
+        a_struct = MyStruct(
+            a_struct.number_1 + 1,
+            a_struct.number_2 + 1,
+            a_struct.number_3 + 1
+        );
+        emit AfterIncrement(msg.sender, a_struct);
     }
 }
