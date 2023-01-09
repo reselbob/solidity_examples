f9a9c17f93254982ece9a54fc79d73b64509566d
Bob Reselman <bob@CogArtTech.com>
2023-01-09 13:42:11 -0800

    Create reselbob_nft_01.sol

---
+++ nfts/reselbob_nft_01.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ReselNFT01 is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter _tokenIds;
+
+    constructor() ERC721("ReselNFT01", "RR1") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return
+            "https://ipfs.io/ipfs/QmdiKjrAYTxdQE936JJCha5GkSWreWjxUf3znVYug4HBzL";
+    }
+
+    function getNewItemId() public view returns(uint256) {
+        return _tokenIds.current();
+    }
+
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
